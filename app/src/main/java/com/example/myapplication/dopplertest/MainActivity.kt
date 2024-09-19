1189c88c42a01b0f69157073d5e4a82f6a2f6f3d
fycz119 <1198117429@qq.com>
2024-09-19 23:09:40 +0800

    add databinding for text

--- app/src/main/java/com/example/myapplication/dopplertest/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/dopplertest/MainActivity.kt
@@ -1,6 +1,7 @@
 
 package com.example.myapplication.dopplertest
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,13 +10,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.dopplertest.jasperlu.doppler.Doppler
 import com.example.myapplication.dopplertest.ui.theme.DopplerTestTheme
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     private val TAG: String = "DopplerTestMainActivity";
@@ -23,6 +29,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lateinit var dopplerText: SnapshotStateList<String>;
         enableEdgeToEdge()
         setContent {
             DopplerTestTheme {
@@ -31,7 +38,10 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                    ShowDopplerText()
+                    dopplerText = remember {
+                        mutableStateListOf<String>()
+                    }
+                    ShowDopplerText(dopplerText)
                 }
             }
         }
@@ -42,19 +52,31 @@ class MainActivity : ComponentActivity() {
         mDoppler.setOnGestureListener(object : Doppler.OnGestureListener{
             override fun onPush() {
                 Log.d(TAG, "onPush: ");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    dopplerText.add(LocalDateTime.now().toString() + "onPush")
+                };
             }
 
             override fun onPull() {
                 Log.d(TAG, "onPull: ");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    dopplerText.add(LocalDateTime.now().toString() + "onPull")
+                };
 
             }
 
             override fun onTap() {
                 Log.d(TAG, "onTap: ");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    dopplerText.add(LocalDateTime.now().toString() + "onTap")
+                };
             }
 
             override fun onDoubleTap() {
                 Log.d(TAG, "onDoubleTap: ");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    dopplerText.add(LocalDateTime.now().toString() + "onDoubleTap")
+                };
             }
 
             override fun onNothing() {
@@ -82,10 +104,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ShowDopplerText(dopplerMessages: MutableList<String>) {
+fun ShowDopplerText(dopplerText: SnapshotStateList<String>) {
     LazyColumn {
-        items() {
-
+        items(dopplerText) { text ->
+            Text(text = text)
         }
     }
 }
